7c1c7921777160c1076ea11281d40be5d62bfec4
Norbert Ruff <norbi@apexlab.io>
2022-08-15 19:32:41 +0200

    [feat] heroku

--- connection.py
+++ connection.py
@@ -4,31 +4,10 @@ import psycopg2
 import psycopg2.extras
 
 
-def get_connection_string():
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    port = os.environ.get('PSQL_PORT')
-    database_name = os.environ.get('PSQL_DB_NAME')
-
-    env_variables_defined = user_name and password and host and database_name
-
-    if env_variables_defined:
-        return 'postgresql://{user_name}:{password}@{host}:{port}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            port=port,
-            database_name=database_name
-        )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
-
-
 def open_database():
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        database_url = os.environ['DATABASE_URL']
+        connection = psycopg2.connect(database_url, sslmode='require')
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
